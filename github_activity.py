85dc19692f10e69d7837d67c5177aa4bfb04eef5
guivera <luizguivera@gmail.com>
2026-06-25 09:22:13 -0300

    Adiciona CLI de atividade Github com README

--- github_activity.py
+++ github_activity.py
@@ -1,30 +1,38 @@
+import sys
 import requests
 
-def github_activity(username):
-    url = f'https://api.github.com/users/{username}/events/public'
+def get_latest_events(username):
+    url = f'https://api.github.com/users/{username}/events'
     response = requests.get(url)
+
     if response.status_code == 200:
-        return response.json()
+        latest_events = response.json()
+        print(latest_events)
+
+        for event in latest_events:
+                if event['type'] == 'IssueCommentEvent':
+                    print(f"Commented on issue {event['payload']['issue']['number']}")
+                elif event['type'] == 'PushEvent':
+                    print(f"Pushed to {event['repo']['name']}")
+                elif event['type'] == 'IssuesEvent':
+                    print(f"Created issue {event['payload']['issue']['number']}")
+                elif event['type'] == 'WatchEvent':
+                    print(f"Starred {event['repo']['name']}")
+                elif event['type'] == 'PullRequestEvent':
+                    print(f"Created pull request {event['payload']['pull_request']['number']}")
+                elif event['type'] == 'PullRequestReviewEvent':
+                    print(f"Reviewed pull request {event['payload']['pull_request']['number']}")
+                elif event['type'] == 'PullRequestReviewCommentEvent':
+                    print(f"Commented on pull request {event['payload']['pull_request']['number']}")
+                elif event['type'] == 'CreateEvent':
+                    print(f"Created {event['payload']['ref_type']} {event['payload']['ref']}")
+                else:
+                    print(f"{event['type']}")
     else:
-        return None
-    
-events = github_activity('luizguivera')
+        print(f"Error fetching events for {username}: {response.status_code}") 
 
-def format_event(event):
-    repo = event["repo"]["name"]
-    type = event["type"]
-
-    if type == "PushEvent":
-        commits = len(event["payload"]["commits"])
-        return f"Pushed {commits} commits to {repo}"
-
-    if type == "WatchEvent":
-        return f"Starred {repo}"
-
-    if type == "CreateEvent":
-        return f"Created something in {repo}"
-
-    return f"{type} in {repo}"
-
-for k, v in events[0].items():
-    print(k, "=>", v)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        get_latest_events(sys.argv[1])
+    else:
+        print("Please provide a GitHub username as a command line argument.")
